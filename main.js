196fc02728cdb0faa3106ea1d206991d379c2d0a
handongxun <handongxun@dcloud.io>
2021-08-23 14:20:45 +0800

    update: 适配VUE3

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
+import App from './App.vue'
+
+const host = 'https://unidemo.dcloud.net.cn/';
+
+// #ifndef VUE3
 import Vue from 'vue'
-import App from './App'
-
 Vue.config.productionTip = false
-
-Vue.prototype.$serverUrl = 'https://unidemo.dcloud.net.cn';
-
+Vue.prototype.$serverUrl = host;
 App.mpType = 'app'
-
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+	createSSRApp
+} from 'vue'
+export function createApp() {
+	const app = createSSRApp(App)
+	app.config.globalProperties.$serverUrl = "https://unidemo.dcloud.net.cn/";
+	return {
+		app
+	}
+}
+// #endif
